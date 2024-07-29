5d7c25042d682949e4bbdba281a62f6b792bdd98
Prophet2023 <83326579+Prophet2023@users.noreply.github.com>
2024-07-29 09:42:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,17 @@ import pandas as pd
 import subprocess
 import tempfile
 
-def print_hi(name):
-
-    print(f'Hi, {name}')  
-
+"""This is a script test if ImmuDef is working correctlt."""
 # Test
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Test start.')
 
     import pandas as pd
     from immune_score.score_caculator import Immune_Score_Caculator
 
     isc = Immune_Score_Caculator()
     # Create a pandas DataFrame
-    df = pd.read_csv('D:/GSEA/SRP318559.csv', index_col=0)
-    data = isc.score_compute(data=df,
-                             data_type='Matrix',
-                             use_all_to_nor=True)
-    print(data)
-
     data = pd.read_csv('Data/data/ssgsea_TB_c7.csv.bz2', index_col=0).transpose()
     isc = Immune_Score_Caculator()
     isc(data=data)
